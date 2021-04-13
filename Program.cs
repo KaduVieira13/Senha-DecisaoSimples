4379f0866290dd9e8e4834be227b7f1089be12ee
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-13 11:10:55 -0300

    Programa finalizado

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Senha_DecisaoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senhaSistema = "1234abcd";
+            string senhaUsuario;
+
+            Console.WriteLine("--- Senha ---\n");
+
+            Console.WriteLine("Seja bem vindo a plataforma!");
+            Console.Write("Digite a senha: ");
+            senhaUsuario = Console.ReadLine().ToLower();
+            
+            
+
+
+            if (senhaSistema != senhaUsuario)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Senha incorreta tente novamente.");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Acesso validado com sucesso!!!");
+            }
+
+            Console.ResetColor();
+
+            
+        }
+    }
+}
